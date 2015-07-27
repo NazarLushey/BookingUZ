fcdfe2cba2fe96ff2c54af105c4966ac23695e6b
OleksiyH <oleksiy.havrylyuk@gmail.com>
2015-07-28 01:17:45 +0400

    Додав окремі змінні по станції відправлення і станції прибуття.

--- src/main/java/mainBooking.java
+++ src/main/java/mainBooking.java
@@ -14,15 +14,20 @@ public class mainBooking {
             result[i] = new train(i);
 
 
-            System.out.println("Train  "+result[i].trainNumber+ "   Departed on " + result[i].dateDeparture + " at "+result[i].hourFrom+":"+result[i].minuteFrom +
+        System.out.println("Train  "+result[i].trainNumber+ "   Departed on " + result[i].dateDeparture + " at "+result[i].hourFrom+":"+result[i].minuteFrom +
                     ".  Arival on "+result[i].dateArival+ " at "+result[i].hourTo+":"+result[i].minuteTo+"." );
 
 
 
         }
-
+        System.out.println();
+        System.out.println("Дані про перший поїзд з списку:");
+        System.out.println("Відправляється з станції: "+result[0].trainFrom);
+        System.out.println("Прибуває на станцію: "+result[0].trainTo);
+        System.out.println("Доступно місць:");
         System.out.println(result[0].seatType1 +"  "+ result[0].seatsAvailable1);
-        System.out.println(result[0].seatType2 +"  "+ result[0].seatsAvailable2);
-        System.out.println(result[0].seatType3 +"  "+ result[0].seatsAvailable3);
+        if(result[0].seatsAvailable2!=0){System.out.println(result[0].seatType2 +"  "+ result[0].seatsAvailable2);} else{}
+        if(result[0].seatsAvailable3!=0){System.out.println(result[0].seatType3 +"  "+ result[0].seatsAvailable3);} else{}
+
     }
 }
